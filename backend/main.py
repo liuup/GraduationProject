b2eb96df9488e97ba08c586ecf37ff4de70b51d5
liuup <akarealup@gmail.com>
2022-06-04 14:53:26 +0800

    修改管理员接口

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pkg_resources import resource_listdir
 
 from pydantic import BaseModel
 # from typing import Optional
@@ -140,13 +141,232 @@ def register(reguser: RegUser):
 ================================================================
 '''
 
-@app.get("/admin/me")
-def adminMe():
-    return failure_json
+'''
+管理员个人信息获取
+'''
+class AdminMe(BaseModel):
+    account: str
+
+@app.post("/admin/me")
+def adminMe(adminme: AdminMe):
+    cnx = mysql.connector.connect(**localdb)
+    # 查询游针
+    cursor = cnx.cursor()
+
+    # print(adminme)
+    # 获取post请求体的字典类型数据
+    adminme_dict = json.loads(adminme.json())
+
+    query_sql = "select * from admins where account = '{}'".format(adminme_dict["account"])
+
+    cursor.execute(query_sql)
+    
+    # 全部数据
+    data = cursor.fetchall()
+    # 数据描述
+    desc = cursor.description
+
+    # 存储单个数据的字典
+    res_dist = {}
+    for i in range(0, len(data[0])):
+        # 将时间序列化
+        if desc[i][0] == "last_logintime":
+            res_dist[desc[i][0]] = str(data[0][i])
+            continue
+        # pass
+        res_dist[desc[i][0]] = data[0][i]
 
 
+    # 关闭连接
+    cursor.close()
+    cnx.close()
+
+    if len(res_dist) == 0:
+        return failure_json
+    else:
+        return json.dumps(res_dist)
 
 
+'''
+管理员个人信息修改接口
+'''
+class AdminEdit(BaseModel):
+    id: str
+    name: str
+    account: str
+    password: str
+    last_logintime: str
+    phone: str
+    
+@app.post("/admin/me/edit")
+def adminMeEdit(adminedit: AdminEdit):
+    adminedit_dict = json.loads(adminedit.json())
+
+    print(adminedit_dict)
+
+    cnx = mysql.connector.connect(**localdb)
+    # 查询游针
+    cursor = cnx.cursor()
+
+    sql = "update admins set name='{}', account='{}', password='{}', phone='{}' where id='{}'"\
+        .format(adminedit_dict["name"],adminedit_dict["account"],adminedit_dict["password"],adminedit_dict["phone"],adminedit_dict["id"])
+    
+    cursor.execute(sql)
+    cnx.commit()
+
+    # 关闭连接
+    cursor.close()
+    cnx.close()
+
+    if cursor.rowcount == 0:
+        return failure_json
+    else:
+        return success_json
+
+
+'''
+================================================================
+#
+# 学生接口
+#
+================================================================
+'''
+class StuMe(BaseModel):
+    account: str
+
+@app.post("/stu/me")
+def stuMe(stume: StuMe):
+    # print(stume)
+    # 获取post请求体的字典类型数据
+    stume_dict = json.loads(stume.json())
+    print(stume_dict)
+
+    cnx = mysql.connector.connect(**localdb)
+    # 查询游针
+    cursor = cnx.cursor()
+
+    # print(adminme)
+
+    query_sql = "select * from students where account = '{}'".format(stume_dict["account"])
+
+    cursor.execute(query_sql)
+    
+    # 全部数据
+    data = cursor.fetchall()
+    # print(data)
+    # 数据描述
+    desc = cursor.description
+
+    # 存储单个数据的字典
+    res_dist = {}
+    for i in range(0, len(data[0])):
+        # 将时间序列化
+        if desc[i][0] == "last_logintime":
+            res_dist[desc[i][0]] = str(data[0][i])
+            continue
+        # pass
+        res_dist[desc[i][0]] = data[0][i]
+
+
+    # 关闭连接
+    cursor.close()
+    cnx.close()
+
+    if len(res_dist) == 0:
+        return failure_json
+    else:
+        return json.dumps(res_dist)
+
+
+'''
+学生个人信息修改接口
+'''
+class StuEdit(BaseModel):
+    id: str
+    name: str
+    account: str
+    password: str
+    last_logintime: str
+    phone: str
+    
+@app.post("/stu/me/edit")
+def adminMeEdit(stuedit: StuEdit):
+    stuedit_dict = json.loads(stuedit.json())
+
+    print(stuedit_dict)
+
+    cnx = mysql.connector.connect(**localdb)
+    # 查询游针
+    cursor = cnx.cursor()
+
+    sql = "update students set name='{}', account='{}', password='{}', phone='{}' where id='{}'"\
+        .format(stuedit_dict["name"],stuedit_dict["account"],stuedit_dict["password"],stuedit_dict["phone"],stuedit_dict["id"])
+    
+    cursor.execute(sql)
+    cnx.commit()
+
+    # 关闭连接
+    cursor.close()
+    cnx.close()
+
+    if cursor.rowcount == 0:
+        return failure_json
+    else:
+        return success_json
+
+
+'''
+================================================================
+#
+# 教师[查询，添加，编辑，删除]接口
+#
+================================================================
+'''
+
+# 获取教师列表
+@app.get("/teacher/list")
+def teacherList():
+    cnx = mysql.connector.connect(**localdb)
+    # 查询游针
+    cursor = cnx.cursor()
+
+    query_sql = "select * from teachers"
+
+    cursor.execute(query_sql)
+    
+    # 全部数据
+    data = cursor.fetchall()
+    # 数据描述
+    desc = cursor.description
+
+    # print(data)
+    # print(desc)
+
+    # 存储全部数据的列表
+    res_list = []
+    
+    for i in range(0, len(data)):
+        # 存储单个数据的字典
+        res_dist = {}
+        for j in range(0, len(data[0])):
+            # 将时间序列化
+            if desc[j][0] == "last_logintime":
+                res_dist[desc[j][0]] = str(data[i][j])
+                continue
+            # pass
+            res_dist[desc[j][0]] = data[i][j]
+        res_list.append(res_dist)
+
+    # print(res_list)
+
+    # 关闭连接
+    cursor.close()
+    cnx.close()
+
+    if len(res_list) == 0:
+        return failure_json
+    else:
+        return json.dumps(res_list)
 
 '''
 ================================================================
